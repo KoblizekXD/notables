e633878fcc4526024ef98b2b54089bfec7caebbf
KoblizekXD <janprokupek@centrum.cz>
2025-03-21 18:03:08 +0100

    Add speedinsights & dark-mode support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import { WaitForMount } from "@/components/wait-for-mount";
 import type { Metadata } from "next";
 import { Inter, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({
   weight: "variable",
@@ -28,8 +31,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} ${jetbrainsMono.variable} antialiased`}>
-        {children}
+        className={`${inter.className} ${jetbrainsMono.variable} antialiased`}
+      >
+        <WaitForMount>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </WaitForMount>
+        <SpeedInsights />
       </body>
     </html>
   );
---
+++ src/components/wait-for-mount.tsx
@@ -0,0 +1,14 @@
+//src\components\ClientRender.tsx
+'use client';
+
+import { type ReactNode, useEffect, useState } from 'react';
+
+export const WaitForMount = ({ children }: { children: ReactNode }) => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
+
+  return <>{children}</>;
+};
