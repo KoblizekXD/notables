47f6850bd3a3cf26c807896f11e1a90c81c985ae
KoblizekXD <janprokupek@centrum.cz>
2025-04-28 17:23:23 +0200

    Sidebar shit

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,15 +1,35 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
+import Logo from "@/components/logo";
+import {
+  Sidebar,
+  SidebarInset,
+  SidebarProvider,
+} from "@/components/ui/sidebar";
 
 export default function Home() {
   return (
-    <Button
-      onClick={async () => {
-        await authClient.signOut();
-      }}>
-      Sign out
-    </Button>
+    <div className="h-screen w-full">
+      <SidebarProvider className="flex flex-col">
+        <div className="h-[var(--header-height)] border-b flex items-center">
+          <Logo />
+        </div>
+        <div className="flex flex-1">
+          <Sidebar className="top-[var(--header-height)] bg-sidebar-primary">
+
+          </Sidebar>
+          <SidebarInset>
+            <div className="flex flex-1 flex-col gap-4 p-4">
+              <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                <div className="aspect-video rounded-xl bg-muted/50" />
+                <div className="aspect-video rounded-xl bg-muted/50" />
+                <div className="aspect-video rounded-xl bg-muted/50" />
+              </div>
+              <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+            </div>
+          </SidebarInset>
+        </div>
+      </SidebarProvider>
+    </div>
   );
 }
--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,14 +4,15 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
 import ThemeBasedRenderer from "./theme-based-renderer";
+import { cn } from "@/lib/utils";
 
-export default function Logo({ animate = true }: { animate?: boolean }) {
+export default function Logo({ animate = true, className }: { animate?: boolean, className?: string }) {
   return (
     <motion.div
       initial={animate ? { opacity: 0, x: "calc(var(--spacing) * -2)" } : {}}
       animate={animate ? { opacity: 1, x: "calc(var(--spacing) * 2)" } : {}}
       transition={{ duration: 0.4, ease: "circInOut" }}
-      className="absolute left-2 top-4">
+      className={cn("absolute left-2", className)}>
       <Link href={"/"}>
         <ThemeBasedRenderer
           light={
