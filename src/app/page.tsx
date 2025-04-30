14804a6762dcb3526bda15b39663e6af49c3eb3a
vojtiikkxdd <vojtarazima@gmail.com>
2025-04-30 16:13:06 +0200

    building a first card for 'why us' section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ import Link from "next/link";
 import { ThemeToggle } from "@/components/theme-toggle";
 import Logo from "@/components/logo";
 import NavMenu from "@/components/landing-page-nav";
-import { ChevronDown } from "lucide-react";
+import { ChevronDown, ArrowUpRight } from "lucide-react";
 
 
 export default function Home() {
   return (
-    <main className="min-h-screen w-screen border items-center flex flex-col ArrowGap">
+    <main className="min-h-screen w-screen max-w-[100vw] border items-center flex flex-col ArrowGap">
       <ThemeBasedRenderer
         light={
           <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]" />
@@ -52,11 +52,40 @@ export default function Home() {
         <Link href="#whyUs" className="flex items-center justify-center px-2 border-2 border-solid border-transparent hover:border-[#d7d7da] dark:hover:border-[#27272a] transition-colors duration-200 rounded-xl cursor-pointer max-w-screen">
           <ChevronDown size={40} />
         </Link>
-        <section id="whyUs" className="flex flex-col items-center justify-center">
+        <section id="whyUs" className="flex flex-col items-center justify-center gap-8">
           <h1 className="font-extrabold text-4xl">Why us?</h1>
           <div className="flex flex-row justify-between px-6 max-w-[90%] flex-wrap">
-            <div className="flex flex-col justify-between px-4 py-6 border-2 border-solid border-[#d7d7da] dark:border-[#27272a] rounded-xl">
+            <div className="flex flex-col justify-between px-4 py-6 border-2 border-solid border-[#d7d7da] dark:border-[#27272a] rounded-xl gap-6">
+              <h1 className="text-4xl font-bold pl-2">1.</h1>
               <h4 className="flex justify-center items-center text-xl font-bold">Create your own notes and games!</h4>
+              <div className="border-2 border-solid border-gray-200 dark:border-[#27272a] rounded-xl flex flex-col justify-between items-center">
+                <div className="flex items-center justify-between w-full p-1 px-2">
+                  <h1 className="text-xl font-bold">Heading</h1>
+                  <div className="flex gap-x-2">
+                    <button
+                      className="cursor-default bg-slate-200 text-[#0F172A] rounded-lg px-2 py-2"
+                      disabled={true}
+                    >
+                      Save
+                    </button>
+                    <button
+                      className="cursor-default bg-slate-900 text-white rounded-lg px-2 py-2
+                    flex items-center"
+                      disabled={true}
+                    >
+                      Post
+                      <ArrowUpRight />
+                    </button>
+                  </div>
+                </div>
+                <hr className="w-full h-0.5 bg-gray-200" />
+                <div className="flex flex-col items-start">
+                  <div className="flex items-center pr-2 py-2">
+                    <p className="border-r-2 border-solid border-black h-6 flex items-center">Lorem ipsum dolor sit amet cons{" "}</p>
+
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </section>
