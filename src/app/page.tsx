cfb538f7068889cf7d92cb3d1bece50993518597
vojtiikkxdd <vojtarazima@gmail.com>
2025-04-30 12:24:11 +0200

    adding redirect to 'why us' section on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,12 @@ import Link from "next/link";
 import { ThemeToggle } from "@/components/theme-toggle";
 import Logo from "@/components/logo";
 import NavMenu from "@/components/landing-page-nav";
+import { ChevronDown } from "lucide-react";
 
 
 export default function Home() {
   return (
-    <main className="min-h-screen w-screen border items-center flex flex-col">
+    <main className="min-h-screen w-screen border items-center flex flex-col ArrowGap">
       <ThemeBasedRenderer
         light={
           <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]" />
@@ -46,6 +47,19 @@ export default function Home() {
             Learn more
           </Link>
         </div>
+      </div>
+      <div className="flex flex-col items-center justify-center gap-16">
+        <Link href="#whyUs" className="flex items-center justify-center px-2 border-2 border-solid border-transparent hover:border-[#d7d7da] dark:hover:border-[#27272a] transition-colors duration-200 rounded-xl cursor-pointer max-w-screen">
+          <ChevronDown size={40} />
+        </Link>
+        <section id="whyUs" className="flex flex-col items-center justify-center">
+          <h1 className="font-extrabold text-4xl">Why us?</h1>
+          <div className="flex flex-row justify-between px-6 max-w-[90%] flex-wrap">
+            <div className="flex flex-col justify-between px-4 py-6 border-2 border-solid border-[#d7d7da] dark:border-[#27272a] rounded-xl">
+              <h4 className="flex justify-center items-center text-xl font-bold">Create your own notes and games!</h4>
+            </div>
+          </div>
+        </section>
       </div>
     </main>
   );
