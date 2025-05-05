95bba9c2770a48fd88f74d2a529aeda492cedb2d
vojtiikkxdd <vojtarazima@gmail.com>
2025-05-05 08:36:56 +0200

    adding cards on dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { ThemeToggle } from "@/components/theme-toggle";
 import Logo from "@/components/logo";
 import NavMenu from "@/components/landing-page-nav";
-import { ChevronDown, ArrowUpRight } from "lucide-react";
+import { ChevronDown, ArrowUpRight, TextCursor } from "lucide-react";
 
 
 export default function Home() {
@@ -49,27 +49,27 @@ export default function Home() {
         </div>
       </div>
       <div className="flex flex-col items-center justify-center gap-16">
-        <Link href="#whyUs" className="flex items-center justify-center px-2 border-2 border-solid border-transparent hover:border-[#d7d7da] dark:hover:border-[#27272a] transition-colors duration-200 rounded-xl cursor-pointer max-w-screen">
+        <Link href="#whyUs" className="flex items-center justify-center px-2 border-2 border-solid border-transparent hover:border-[#d7d7da] dark:hover:border-zinc-800 transition-colors duration-200 rounded-xl cursor-pointer max-w-screen">
           <ChevronDown size={40} />
         </Link>
         <section id="whyUs" className="flex flex-col items-center justify-center gap-8">
           <h1 className="font-extrabold text-4xl">Why us?</h1>
-          <div className="flex flex-row justify-between px-6 max-w-[90%] flex-wrap">
-            <div className="flex flex-col justify-between px-4 py-6 border-2 border-solid border-[#d7d7da] dark:border-[#27272a] rounded-xl gap-6">
+          <div className="flex flex-row justify-between px-6 flex-wrap gap-12">
+            <div className="flex flex-col justify-between px-4 py-6 border-2 border-solid border-[#d7d7da] dark:border-zinc-800 rounded-xl gap-6 max-w-84">
               <h1 className="text-4xl font-bold pl-2">1.</h1>
               <h4 className="flex justify-center items-center text-xl font-bold">Create your own notes and games!</h4>
-              <div className="border-2 border-solid border-gray-200 dark:border-[#27272a] rounded-xl flex flex-col justify-between items-center">
+              <div className="border-2 border-solid border-gray-200 dark:border-zinc-800 rounded-xl flex flex-col justify-between items-center">
                 <div className="flex items-center justify-between w-full p-1 px-2">
-                  <h1 className="text-xl font-bold">Heading</h1>
+                  <h1 className="text-xl font-bold select-none">Heading</h1>
                   <div className="flex gap-x-2">
                     <button
-                      className="cursor-default bg-slate-200 text-[#0F172A] rounded-lg px-2 py-2"
+                      className="cursor-default bg-slate-200 select-none text-[#0F172A] rounded-lg px-2 py-2"
                       disabled={true}
                     >
                       Save
                     </button>
                     <button
-                      className="cursor-default bg-slate-900 text-white rounded-lg px-2 py-2
+                      className="cursor-default bg-slate-900 select-none text-white rounded-lg px-2 py-2
                     flex items-center"
                       disabled={true}
                     >
@@ -78,12 +78,82 @@ export default function Home() {
                     </button>
                   </div>
                 </div>
-                <hr className="w-full h-0.5 bg-gray-200" />
-                <div className="flex flex-col items-start">
-                  <div className="flex items-center pr-2 py-2">
-                    <p className="border-r-2 border-solid border-black h-6 flex items-center">Lorem ipsum dolor sit amet cons{" "}</p>
-
+                <hr className="w-full h-0.5 bg-gray-200 dark:bg-zinc-800" />
+                <div className="flex flex-col items-start relative min-h-30">
+                  <div className="flex items-center pr-2 py-2 ">
+                    <p className="border-r-2 border-solid border-black select-none h-6 flex items-center">
+                      Lorem ipsum dolor sit amet cons{" "}
+                    </p>
                   </div>
+                  <TextCursor className="absolute right-40 top-10" />
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col justify-between px-4 py-6 border-2 border-solid border-[#d7d7da] dark:border-zinc-800 rounded-xl gap-6 max-w-84">
+              <h1 className="text-4xl font-bold pl-2">1.</h1>
+              <h4 className="flex justify-center items-center text-xl font-bold">Create your own notes and games!</h4>
+              <div className="border-2 border-solid border-gray-200 dark:border-zinc-800 rounded-xl flex flex-col justify-between items-center">
+                <div className="flex items-center justify-between w-full p-1 px-2">
+                  <h1 className="text-xl font-bold select-none">Heading</h1>
+                  <div className="flex gap-x-2">
+                    <button
+                      className="cursor-default bg-slate-200 select-none text-[#0F172A] rounded-lg px-2 py-2"
+                      disabled={true}
+                    >
+                      Save
+                    </button>
+                    <button
+                      className="cursor-default bg-slate-900 select-none text-white rounded-lg px-2 py-2
+                    flex items-center"
+                      disabled={true}
+                    >
+                      Post
+                      <ArrowUpRight />
+                    </button>
+                  </div>
+                </div>
+                <hr className="w-full h-0.5 bg-gray-200 dark:bg-zinc-800" />
+                <div className="flex flex-col items-start relative min-h-30">
+                  <div className="flex items-center pr-2 py-2 ">
+                    <p className="border-r-2 border-solid border-black select-none h-6 flex items-center">
+                      Lorem ipsum dolor sit amet cons{" "}
+                    </p>
+                  </div>
+                  <TextCursor className="absolute right-40 top-10" />
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col justify-between px-4 py-6 border-2 border-solid border-[#d7d7da] dark:border-zinc-800 rounded-xl gap-6 max-w-84">
+              <h1 className="text-4xl font-bold pl-2">1.</h1>
+              <h4 className="flex justify-center items-center text-xl font-bold">Create your own notes and games!</h4>
+              <div className="border-2 border-solid border-gray-200 dark:border-zinc-800 rounded-xl flex flex-col justify-between items-center">
+                <div className="flex items-center justify-between w-full p-1 px-2">
+                  <h1 className="text-xl font-bold select-none">Heading</h1>
+                  <div className="flex gap-x-2">
+                    <button
+                      className="cursor-default bg-slate-200 select-none text-[#0F172A] rounded-lg px-2 py-2"
+                      disabled={true}
+                    >
+                      Save
+                    </button>
+                    <button
+                      className="cursor-default bg-slate-900 select-none text-white rounded-lg px-2 py-2
+                    flex items-center"
+                      disabled={true}
+                    >
+                      Post
+                      <ArrowUpRight />
+                    </button>
+                  </div>
+                </div>
+                <hr className="w-full h-0.5 bg-gray-200 dark:bg-zinc-800" />
+                <div className="flex flex-col items-start relative min-h-30">
+                  <div className="flex items-center pr-2 py-2 ">
+                    <p className="border-r-2 border-solid border-black select-none h-6 flex items-center">
+                      Lorem ipsum dolor sit amet cons{" "}
+                    </p>
+                  </div>
+                  <TextCursor className="absolute right-40 top-10" />
                 </div>
               </div>
             </div>
