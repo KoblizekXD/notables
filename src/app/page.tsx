1b6bbfffe51bcb056d7aa382488c28853ef30ba3
vojtiikkxdd <vojtarazima@gmail.com>
2025-05-06 09:24:42 +0200

    finishing why us cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,8 @@ import Link from "next/link";
 import { ThemeToggle } from "@/components/theme-toggle";
 import Logo from "@/components/logo";
 import NavMenu from "@/components/landing-page-nav";
-import { CornerLeftDown, ArrowUpRight, TextCursor, Check } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { CornerLeftDown, ArrowUpRight, TextCursor, Check, CalendarDays } from "lucide-react";
 
 
 export default function Home() {
@@ -21,7 +22,7 @@ export default function Home() {
         <NavMenu className="navMenu transition-opacity duration-150 ease-in-out" />
       </div>
       <ThemeToggle className="absolute right-2 top-2" />
-      <div className="text-center lg:mt-[30vh] md:mt-[24vh] mt-[18vh] flex gap-y-3 flex-col items-center md:p-4 px-6">
+      <div className="text-center lg:mt-[30vh] md:mt-[24vh] mt-[18vh] flex gap-y-10 md:gap-y-3 flex-col items-center md:p-4 px-6">
         <div className="flex flex-col items-center gap-2">
           <Link
             href={"/docs/api"}
@@ -44,35 +45,36 @@ export default function Home() {
             <ArrowRight />
             <span>Get started</span>
           </LinkingButton>
-          <Link className="underline font-semibold hover:text-[#a086e7] duration-250 text-accent-foreground" href={"/#more"} passHref>
+          <Link className=" underline font-semibold hover:text-[#a086e7] duration-250 text-accent-foreground" 
+            href={"/#more"} passHref>
             Learn more
           </Link>
         </div>
       </div>
       <div className="flex flex-col items-center justify-center gap-32">
-        <div className="flex flex-col items-center justify-center gap-32">
+        <div className="flex flex-col  items-center justify-center gap-32">
           <h1 className="font-extrabold text-4xl flex gap-4 relative">
             <CornerLeftDown size={50} className="absolute top-2 -left-15" />
             Why us?
           </h1>
-          <div className="grid lg:flex-row lg:grid-cols-3 grid-cols-1 justify-between px-6 flex-wrap gap-12">
+          <div className="grid max-w-screen lg:flex-row lg:grid-cols-3 grid-cols-1 justify-between px-6 flex-wrap gap-12">
             <div className="flex flex-col justify-between px-4 py-6 border-2 border-solid border-[#d7d7da] dark:border-zinc-800 rounded-xl gap-6 max-w-84">
               <h1 className="text-4xl font-bold pl-2">1.</h1>
               <h4 className="flex justify-center items-center text-xl font-bold">Create your own notes and games</h4>
-              <div className="border-2 border-solid border-gray-200 dark:border-zinc-800 rounded-xl flex flex-col justify-between items-center">
+              <div className="border-2 border-solid border-gray-200 dark:border-zinc-800 rounded-xl flex flex-col justify-between items-center shadow-md min-w-[265px]">
                 <div className="flex items-center justify-between w-full p-1 px-2">
                   <h1 className="text-xl font-bold select-none">Heading</h1>
                   <div className="flex gap-x-2">
                     <button
                       type="button"
-                      className="cursor-default bg-slate-200 select-none text-[#0F172A] rounded-lg px-2 py-2"
+                      className="cursor-default bg-slate-200 hover:bg-slate-400 transition-colors duration-200 select-none text-[#0F172A] rounded-lg px-2 py-2"
                       disabled={true}
                     >
                       Save
                     </button>
                     <button
                       type="button"
-                      className="cursor-default bg-slate-900 select-none text-white rounded-lg px-2 py-2 flex items-center"
+                      className="cursor-default bg-slate-900 hover:bg-slate-950 transition-colors duration-200 select-none text-white rounded-lg px-2 py-2 flex items-center"
                       disabled={true}
                     >
                       Post
@@ -94,25 +96,38 @@ export default function Home() {
             <div className="flex flex-col justify-between px-4 py-6 border-2 border-solid border-[#d7d7da] dark:border-zinc-800 rounded-xl gap-6 max-w-84">
               <h1 className="text-4xl font-bold pl-2">2.</h1>
               <h4 className="flex justify-center items-center text-xl font-bold">Share knowledge with others</h4>
-              <div className="border-2 border-solid border-gray-200 dark:border-zinc-800 rounded-xl flex flex-col justify-between items-center">
-
+              <div className="border-2 border-solid border-gray-200 dark:border-zinc-800 rounded-xl flex flex-row justify-between items-start shadow-md lg:p-5 p-3 gap-x-3 min-w-[265px]">
+                <Avatar>
+                  <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                  <AvatarFallback>idk</AvatarFallback>
+                </Avatar>
+                <div className="flex flex-col gap-y-3">
+                  <div className="flex flex-col gap-y-1 items-start">
+                    <h1 className="font-bold text-xl">Random user</h1>
+                    <p className="leading-4">Munich Agreement - Lorem ipsum dolor sit amet</p>
+                  </div>
+                  <div className="flex flex-row gap-x-2 items-center">
+                    <CalendarDays size={18} color="#a3a3a3" />
+                    <p className="text-sm whitespace-nowrap">Created 24. June 2025</p>
+                  </div>
+                </div>
               </div>
             </div>
             <div className="flex flex-col justify-between px-4 py-6 border-2 border-solid border-[#d7d7da] dark:border-zinc-800 rounded-xl gap-6 max-w-84">
               <h1 className="text-4xl font-bold pl-2">3.</h1>
               <h4 className="flex justify-center items-center text-xl font-bold">Play games and learn efficiently</h4>
-              <div className="border-2 border-solid border-gray-200 dark:border-zinc-800 rounded-xl flex flex-col justify-between items-center">
+              <div className="border-2 border-solid border-gray-200 dark:border-zinc-800 rounded-xl flex flex-col justify-between items-center shadow-md min-w-[265px]">
                 <div className="grid grid-cols-2 gap-2 p-1 px-2">
-                  <div className="bg-[#a3a3a3] p-5 lg:p-3 items-center justify-center flex border-2 border-solid border-[#777777] rounded-xl">
+                  <div className="bg-[#a3a3a3] p-5 lg:py-6 lg:p-3 items-center justify-center flex border-2 border-solid border-[#777777] rounded-xl">
                     Albert Einstein
                   </div>
-                  <div className="bg-[#5DEE83] p-5 lg:p-3 items-center justify-center flex border-2 border-solid border-[#47a660] rounded-xl">
-                    <Check color="#47a660" />
+                  <div className="bg-[#5DEE83] p-5 lg:py-6 lg:p-3 items-center justify-center flex border-2 border-solid border-[#47a660] rounded-xl">
+                    <Check color="#47a660" size={40} />
                   </div>
-                  <div className="bg-[#5DEE83] p-5 lg:p-3 items-center justify-center flex border-2 border-solid border-[#47a660] rounded-xl">
-                    <Check color="#47a660" />
+                  <div className="bg-[#5DEE83] p-5 lg:py-6 lg:p-3 items-center justify-center flex border-2 border-solid border-[#47a660] rounded-xl">
+                    <Check color="#47a660" size={40} />
                   </div>
-                  <div className="bg-[#a3a3a3] p-5 lg:p-3 items-center justify-center flex border-2 border-solid border-[#777777] rounded-xl">
+                  <div className="bg-[#a3a3a3] p-5 lg:py-6 lg:p-3 items-center justify-center flex border-2 border-solid border-[#777777] rounded-xl">
                     Albert Einstein
                   </div>
                 </div>
@@ -121,6 +136,9 @@ export default function Home() {
           </div>
         </div>
       </div>
-    </main>
+      <footer>
+        
+      </footer>
+    </main >
   );
 }
