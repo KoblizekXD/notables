09a4064184d0bf7560998ba8a05eb2e5ebbdf48e
vojtiikkxdd <vojtarazima@gmail.com>
2025-05-06 13:49:25 +0200

    fixing aligning cards content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import { CornerLeftDown, ArrowUpRight, TextCursor, Check, CalendarDays } from "l
 
 export default function Home() {
   return (
-    <main className="min-h-screen w-screen max-w-screen border items-center flex flex-col lg:gap-28 md:gap-38 gap-60 overflow-auto">
+    <main className="min-h-screen w-screen max-w-screen items-center flex flex-col lg:gap-28 md:gap-38 gap-60 ">
       <ThemeBasedRenderer
         light={
           <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]" />
@@ -45,99 +45,97 @@ export default function Home() {
             <ArrowRight />
             <span>Get started</span>
           </LinkingButton>
-          <Link className=" underline font-semibold hover:text-[#a086e7] duration-250 text-accent-foreground" 
+          <Link className=" underline font-semibold hover:text-[#a086e7] duration-250 text-accent-foreground"
             href={"/#more"} passHref>
             Learn more
           </Link>
         </div>
       </div>
       <div className="flex flex-col items-center justify-center gap-32">
-        <div className="flex flex-col  items-center justify-center gap-32">
-          <h1 className="font-extrabold text-4xl flex gap-4 relative">
-            <CornerLeftDown size={50} className="absolute top-2 -left-15" />
-            Why us?
-          </h1>
-          <div className="grid max-w-screen lg:flex-row lg:grid-cols-3 grid-cols-1 justify-between px-6 flex-wrap gap-12">
-            <div className="flex flex-col justify-between px-4 py-6 border-2 border-solid border-[#d7d7da] dark:border-zinc-800 rounded-xl gap-6 max-w-84">
-              <h1 className="text-4xl font-bold pl-2">1.</h1>
-              <h4 className="flex justify-center items-center text-xl font-bold">Create your own notes and games</h4>
-              <div className="border-2 border-solid border-gray-200 dark:border-zinc-800 rounded-xl flex flex-col justify-between items-center shadow-md min-w-[265px]">
-                <div className="flex items-center justify-between w-full p-1 px-2">
-                  <h1 className="text-xl font-bold select-none">Heading</h1>
-                  <div className="flex gap-x-2">
-                    <button
-                      type="button"
-                      className="cursor-default bg-slate-200 hover:bg-slate-400 transition-colors duration-200 select-none text-[#0F172A] rounded-lg px-2 py-2"
-                      disabled={true}
-                    >
-                      Save
-                    </button>
-                    <button
-                      type="button"
-                      className="cursor-default bg-slate-900 hover:bg-slate-950 transition-colors duration-200 select-none text-white rounded-lg px-2 py-2 flex items-center"
-                      disabled={true}
-                    >
-                      Post
-                      <ArrowUpRight />
-                    </button>
-                  </div>
-                </div>
-                <hr className="w-full h-0.5 bg-gray-200 dark:bg-zinc-800" />
-                <div className="flex flex-col items-start relative min-h-30">
-                  <div className="flex items-center pr-2 py-2 ">
-                    <p className="select-none h-6 p-2 md:pt-5 flex items-center">
-                      Lorem ipsum dolor sit amet cons | {/* <span className="animate-pulse">|</span> */}
-                    </p>
-                  </div>
-                  <TextCursor className="absolute right-40 top-10" />
+        <h1 className="font-extrabold text-4xl flex gap-4 relative">
+          <CornerLeftDown size={50} className="absolute top-2 -left-15" />
+          Why us?
+        </h1>
+        <div className="grid max-w-screen lg:flex-row lg:grid-cols-3 grid-cols-1 justify-between px-6 gap-12">
+          <div className="flex flex-col justify-between px-4 py-6 border-2 border-solid border-[#d7d7da] dark:border-zinc-800 rounded-xl gap-6 lg:max-w-84">
+            <h1 className="text-4xl font-bold pl-2">1.</h1>
+            <h4 className="flex justify-center items-center text-xl font-bold">Create your own notes and games</h4>
+            <div className="border-2 border-solid border-gray-200 dark:border-zinc-800 rounded-xl flex flex-col justify-between items-center shadow-md min-w-[265px] min-h-[202px]">
+              <div className="flex items-center justify-between w-full p-1 px-2">
+                <h1 className="text-xl font-bold select-none">Heading</h1>
+                <div className="flex gap-x-2">
+                  <button
+                    type="button"
+                    className="cursor-default bg-slate-200 hover:bg-slate-400 transition-colors duration-200 select-none text-[#0F172A] rounded-lg px-2 py-2"
+                    disabled={true}
+                  >
+                    Save
+                  </button>
+                  <button
+                    type="button"
+                    className="cursor-default bg-slate-900 hover:bg-slate-950 transition-colors duration-200 select-none text-white rounded-lg px-2 py-2 flex items-center"
+                    disabled={true}
+                  >
+                    Post
+                    <ArrowUpRight />
+                  </button>
                 </div>
               </div>
+              <hr className="w-full h-0.5 bg-gray-200 dark:bg-zinc-800" />
+              <div className="flex flex-col items-start relative min-h-30">
+                <div className="flex items-center pr-2 py-2 ">
+                  <p className="select-none h-6 p-2 md:pt-5 flex items-center">
+                    Lorem ipsum dolor sit amet cons | {/* <span className="animate-pulse">|</span> */}
+                  </p>
+                </div>
+                <TextCursor className="absolute right-40 top-10" />
+              </div>
             </div>
-            <div className="flex flex-col justify-between px-4 py-6 border-2 border-solid border-[#d7d7da] dark:border-zinc-800 rounded-xl gap-6 max-w-84">
+          </div>
+          <div className="flex flex-col justify-between px-4 py-6 border-2 border-solid border-[#d7d7da] dark:border-zinc-800 rounded-xl lg:max-w-84">
+            <div className="flex flex-col gap-6">
               <h1 className="text-4xl font-bold pl-2">2.</h1>
               <h4 className="flex justify-center items-center text-xl font-bold">Share knowledge with others</h4>
-              <div className="border-2 border-solid border-gray-200 dark:border-zinc-800 rounded-xl flex flex-row justify-between items-start shadow-md lg:p-5 p-3 gap-x-3 min-w-[265px]">
-                <Avatar>
-                  <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                  <AvatarFallback>idk</AvatarFallback>
-                </Avatar>
-                <div className="flex flex-col gap-y-3">
-                  <div className="flex flex-col gap-y-1 items-start">
-                    <h1 className="font-bold text-xl">Random user</h1>
-                    <p className="leading-4">Munich Agreement - Lorem ipsum dolor sit amet</p>
-                  </div>
-                  <div className="flex flex-row gap-x-2 items-center">
-                    <CalendarDays size={18} color="#a3a3a3" />
-                    <p className="text-sm whitespace-nowrap">Created 24. June 2025</p>
-                  </div>
+            </div>
+            <div className="border-2 border-solid border-gray-200 dark:border-zinc-800 rounded-xl flex flex-row items-start shadow-md lg:p-5 p-3 gap-x-3 min-w-[265px]">
+              <Avatar>
+                <AvatarImage src="./pfp.png" alt="@shadcn" />
+                <AvatarFallback>idk</AvatarFallback>
+              </Avatar>
+              <div className="flex flex-col gap-y-3">
+                <div className="flex flex-col gap-y-1 items-start">
+                  <h1 className="font-bold text-xl">Random user</h1>
+                  <p className="leading-4">Munich Agreement - Lorem ipsum dolor sit amet</p>
+                </div>
+                <div className="flex flex-row gap-x-2 items-center">
+                  <CalendarDays size={18} color="#a3a3a3" />
+                  <p className="text-sm whitespace-nowrap">Created 24. June 2025</p>
                 </div>
               </div>
             </div>
-            <div className="flex flex-col justify-between px-4 py-6 border-2 border-solid border-[#d7d7da] dark:border-zinc-800 rounded-xl gap-6 max-w-84">
-              <h1 className="text-4xl font-bold pl-2">3.</h1>
-              <h4 className="flex justify-center items-center text-xl font-bold">Play games and learn efficiently</h4>
-              <div className="border-2 border-solid border-gray-200 dark:border-zinc-800 rounded-xl flex flex-col justify-between items-center shadow-md min-w-[265px]">
-                <div className="grid grid-cols-2 gap-2 p-1 px-2">
-                  <div className="bg-[#a3a3a3] p-5 lg:py-6 lg:p-3 items-center justify-center flex border-2 border-solid border-[#777777] rounded-xl">
-                    Albert Einstein
-                  </div>
-                  <div className="bg-[#5DEE83] p-5 lg:py-6 lg:p-3 items-center justify-center flex border-2 border-solid border-[#47a660] rounded-xl">
-                    <Check color="#47a660" size={40} />
-                  </div>
-                  <div className="bg-[#5DEE83] p-5 lg:py-6 lg:p-3 items-center justify-center flex border-2 border-solid border-[#47a660] rounded-xl">
-                    <Check color="#47a660" size={40} />
-                  </div>
-                  <div className="bg-[#a3a3a3] p-5 lg:py-6 lg:p-3 items-center justify-center flex border-2 border-solid border-[#777777] rounded-xl">
-                    Albert Einstein
-                  </div>
-                </div>
+          </div>
+          <div className="flex flex-col justify-between px-4 py-6 border-2 border-solid border-[#d7d7da] dark:border-zinc-800 rounded-xl gap-6 lg:max-w-84">
+            <h1 className="text-4xl font-bold pl-2">3.</h1>
+            <h4 className="flex justify-center items-center text-xl font-bold">Play games and learn efficiently</h4>
+            <div className="border-2 border-solid border-gray-200 dark:border-zinc-800 rounded-xl items-center shadow-md min-w-[265px] min-h-[202px] justify-center grid grid-cols-2 grid-rows-2 gap-2 p-1 px-2">
+              <div className="bg-[#a3a3a3] h-20  items-center justify-center flex border-2 border-solid border-[#777777] rounded-xl">
+                Albert Einstein
+              </div>
+              <div className="bg-[#5DEE83] h-20  items-center justify-center flex border-2 border-solid border-[#47a660] rounded-xl">
+                <Check color="#47a660" size={30} />
+              </div>
+              <div className="bg-[#5DEE83] h-20  items-center justify-center flex border-2 border-solid border-[#47a660] rounded-xl">
+                <Check color="#47a660" size={30} />
+              </div>
+              <div className="bg-[#a3a3a3] h-20  items-center justify-center flex border-2 border-solid border-[#777777] rounded-xl">
+                Albert Einstein
               </div>
             </div>
           </div>
         </div>
       </div>
       <footer>
-        
+
       </footer>
     </main >
   );
